61d28c58b46124f3fba950eb6e8db6c66586f311
MigAnette <43532669+MigAnette@users.noreply.github.com>
2020-05-12 12:33:52 +0200

    added router push to exercise card

--- src/components/cards/PickExerciseCard.vue
+++ src/components/cards/PickExerciseCard.vue
@@ -1,31 +1,34 @@
 <template>
   <v-card class="ma-4" @click="sendData">
-      <v-img :src="content.image" aspect-ratio="1.6"></v-img>
-      <v-card-title>{{content.name}} </v-card-title>
+    <v-img :src="content.image" aspect-ratio="1.6"></v-img>
+    <v-card-title>{{content.name}}</v-card-title>
   </v-card>
 </template>
 
 <script>
 export default {
-  name: 'PickExerciseCard',
+  name: "PickExerciseCard",
   props: ["content"],
   data() {
-  return {
-  };
- },
- methods: {
-     sendData() {
-        const data = {
-            image: this.content.image,
-            name: this.content.name,
-            repsAndSets: this.content.repsAndSets,
-            description: this.content.description
-        };
+    return {
+      routeName: "MakeRoutine"
+    };
+  },
+  methods: {
+    sendData() {
+      const data = {
+        image: this.content.image,
+        name: this.content.name,
+        repsAndSets: this.content.repsAndSets,
+        description: this.content.description
+      };
 
-        this.$store.commit("SET_EXERCISE_TEST", data);
-
-     }
- }
+      this.$store.commit("SET_EXERCISE_TEST", data);
+      this.$router.push({
+        name: this.routeName
+      });
+    }
+  }
 };
 </script>
 
