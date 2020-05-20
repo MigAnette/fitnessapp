c294f8250251d0324eb0513cbc351c294cc8177b
MigAnette <43532669+MigAnette@users.noreply.github.com>
2020-05-20 11:36:11 +0200

    Update user.spec.js

--- tests/unit/user.spec.js
+++ tests/unit/user.spec.js
@@ -1,10 +1,4 @@
-import Vuex from 'vuex'
 import user from '@/store/modules/user'
-import { createLocalVue } from '@vue/test-utils'
-// import cloneDeep from 'lodash.clonedeep'
-
-const localVue = createLocalVue()
-localVue.use(Vuex)
 
 describe('store/modules/user.js', () => {
  
